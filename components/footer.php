9f934bffe3a4cf0e91777b8217e99df1d3cb153b
TuyetAnhhhh <tuyetanh.0867@gmail.com>
2024-10-18 11:21:29 +0700

    tiekank_18_10_2
    Them footer

---
+++ components/footer.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
+<body>
+
+    <!-- Nội dung trang ở đây -->
+
+    <!-- Footer bắt đầu -->
+    <footer class="bg-dark text-white mt-5 p-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>About Us</h5>
+                <p>Short description about the company or website.</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Home</a></li>
+                    <li><a href="#" class="text-white">Services</a></li>
+                    <li><a href="#" class="text-white">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 text-center">
+                <h5>Follow Us</h5>
+                <a href="#"><i class="bi bi-facebook"></i></a>
+                <a href="#"><i class="bi bi-twitter"></i></a>
+                <a href="#"><i class="bi bi-instagram"></i></a>
+            </div>
+        </div>
+        <div class="text-center mt-3">
+        <p>&copy; <?php echo date('Y'); ?> Your Website. All rights reserved.</p>
+
+        </div>
+    </div>
+</footer>
+
+
+    <!-- Footer kết thúc -->
+
+    <!-- Bootstrap JS and Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"></script>
+    <script src="../js/header.js"></script>
+  <script src="../js/dark-mode.js"></script>
+</body>
+</html>
