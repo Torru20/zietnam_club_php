d64244987a2ef55a5eeee711117fc4a85d05b62f
ニュー とる <torrunhu20@gmail.com>
2024-10-21 10:45:15 +0700

    nhu_21/10_1
    chỉnh header thành navbar (bootstrap

---
+++ components/nav_bar.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" href="../css/nav_bar.css">
+
+<nav class="navbar navbar-expand-lg ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../images/logo.png" alt="zietnamclub" width="30" height="30">
+    </a>
+    <a class="navbar-brand" href="#">Zietnam Club</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="../pages/index.php">H O M E</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">S T U D Y</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            C L U B
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">F O R U M</a></li>
+            <li><a class="dropdown-item" href="#">C H A T C H I T</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Do you need some help?</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">S E T T I N G S</a>
+        </li>
+        
+      </ul>
+        <div class="header-main-sm">
+            <a href="https://www.facebook.com/saigontranslatorteambyjulie"><i class="fa-brands fa-facebook"></i></a>
+            <a href="https://www.xiaohongshu.com/user/profile/627362150000000021020c68"><div class="header-main-sm-in"></div></a>
+            <i id="change-mode" class="fas fa-moon"></i>
+
+        </div>
+    </div>
+  </div>
+</nav>
+<script src="../js/header.js"></script>
+<script src="../js/dark-mode.js"></script>
