e8aeb3a0d2e0741a947c55d33d20aac544f5bb75
ニュー とる <torrunhu20@gmail.com>
2024-10-23 17:28:16 +0700

    nhu_23/10_5
    chat page all, check some little things...

--- components/chat_user.php
+++ components/chat_user.php
@@ -1,10 +1,35 @@
+<style>
+    .list-group-item-action{
+        color:var(--text);
+        background-color:var(--surface-color);
+    }
+    .list-group-item-action:hover{
+        color:var(--text);
+        background-color:var(--secondary-color);
+    }
+    a.list-group-item.list-group-item-action.active{
+        color:var(--surface-color);
+        background-color:var(--primary-color);
+        border-color:var(--primary-color);
+    }
+    a.list-group-item.list-group-item-action.active:hover{
+        color:var(--text);
+        background-color:var(--secondary-color);
+    }
+    .text-bg-primary.rounded-pill {
+        background-color: var(--secondary-color) !important; 
+    }
+    
+
+</style>
+
 <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
     <div class="d-flex w-100 justify-content-between">
-    <h5 class="mb-1">List group item heading</h5>
-    <small>3 days ago</small>
+    <h5 class="mb-1">Nhu</h5>
+    <small>1 days ago</small>
     </div>
-    <p class="mb-1">Some placeholder content in a paragraph.</p>
-    <span class="badge text-bg-primary rounded-pill">14</span>
+    <p class="mb-1">Hi there.</p>
+    <span class="badge text-bg-primary rounded-pill">1</span>
 </a>
 <a href="#" class="list-group-item list-group-item-action">
     <div class="d-flex w-100 justify-content-between">
--- components/chat_user_list.php
+++ components/chat_user_list.php
@@ -1,23 +1,42 @@
 <style>
-    .offcanvas-start {
-        margin-top: 60px;
+    .chat-user-list .btn-primary{
+        background-color:var(--primary-color);
+        border-color:var(--primary-color);
     }
+    .chat-user-list .btn-primary:hover{
+        background-color:var(--inverseprimary-color);
+        border-color:var(--inverseprimary-color);
+    }
+    .offcanvas-start {
+        margin-top: 55px;
+        background-color:var(--surface-color);
+    }
+    .offcanvas-header h5{
+        color:var(--primary-color);
+    }
+    .offcanvas-body p{
+        color:var(--text);
+    }
+
+
 </style>
+<div class="chat-user-list">
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"><i class="fa-solid fa-chevron-left"></i></button>
 
-<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"><i class="fa-solid fa-chevron-left"></i></button>
-
-<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-    <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body">
-        <p>Try scrolling the rest of the page to see this option in action.</p>
-        <div class="list-group">
-            <?php
-            include "../components/chat_user.php";
-            ?>
+    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Chat Chit</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <p>Try to share cheap moment with your friend</p>
+            <div class="list-group">
+                <?php
+                include "../components/chat_user.php";
+                ?>
+            </div>
         </div>
     </div>
 </div>
+
 
