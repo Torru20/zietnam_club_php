cef460fffcddd1ead00258d5bd7e456957b92e4f
ニュー とる <torrunhu20@gmail.com>
2024-10-23 08:45:45 +0700

    nhu_23/10_1
    setting_page to bootstrap

--- pages/settings_page.php
+++ pages/settings_page.php
@@ -22,29 +22,36 @@
     <div class="settings_container">
         <div class="settings_images_id">
             <img src="../images/Hinhdaidien.jpg" alt="User Avatar">
-            <h4>ID:</h4>
+            <input id="box-user-Id" class="form-control" type="text" placeholder="User ID" aria-label="Disabled input example" disabled>
         </div>
+    </div>
 
-       
-            <div class="settings_info_user">
-                <p>
-                    Name:<br>
-                    Email:<br>
-                    Phone:<br>
-                    Address:
-                </p>
-            </div>
-       
-
-     </div>
-     <div class="setting_btn">
-        <div class="settings_button">
-            <a href="login_page.php" class="button">Sign out</a>
-         </div>
-         <div class="settings_transfer">
-            <a href="login_page.php" class="button">Transfer accounts</a>
+    <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="floatingInputDisabled" placeholder="name@example.com" disabled>
+        <label for="floatingInputDisabled">Name</label>
         </div>
-     </div>
+            <div class="form-floating mb-3">
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextareaDisabled" disabled></textarea>
+            <label for="floatingTextareaDisabled">Email</label>
+        </div>
+        <div class="form-floating mb-3">
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2Disabled" style="height: 100px" disabled>Disabled textarea with some text inside</textarea>
+            <label for="floatingTextarea2Disabled">Bio</label>
+        </div>
+        <div class="form-floating">
+            <select class="form-select" id="floatingSelectDisabled" aria-label="Floating label disabled select example" disabled>
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        <label for="floatingSelectDisabled">Works with selects</label>
+    </div>
+
+    <div class="setting_btn">
+        <a class="btn btn-outline-primary" href="#" role="button">Sign out</a>
+        <button class="btn btn-secondary" type="submit">Change Profile</button>
+    </div>
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
