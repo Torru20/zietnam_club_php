884eb98da3d162bad7944f4d576ec24ffd9a3475
ニュー とる <torrunhu20@gmail.com>
2024-11-06 21:48:57 +0700

    nhu_6/11_4

--- pages/login_page.php
+++ pages/login_page.php
@@ -37,30 +37,7 @@
                 </form>
             </div>
 
-            <?php
-            if ($_SERVER['REQUEST_METHOD'] == "GET" && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
-            header('Location: forum_page.php');
-            }
-            if(isset($_POST['login']) && !empty($_POST['login'])) {
-                $email = $_POST['email'];
-                $password = $_POST['password'];
-
-                if(!empty($email) or !empty($password)) {
-                $email = $getFromU->checkInput($email);
-                $password = $getFromU->checkInput($password);
-
-                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                    $errorMsg = "Invalid format";
-                }else {
-                    if($getFromU->login($email, $password) === false){
-                    $errorMsg = "The email or password is incorrect!";
-                    }
-                }
-                }else {
-                $errorMsg = "Please enter username and password!";
-                }
-            }
-            ?>
+            
             <div class="form-container sign-in">
                 <form method="post">
                     <h1>Log in</h1>
