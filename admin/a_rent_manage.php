ced82b063f2c5e2b44902353af3215f78e42ec48
ニュー とる <torrunhu20@gmail.com>
2024-12-19 11:45:44 +0700

    Update a_rent_manage.php

--- admin/a_rent_manage.php
+++ admin/a_rent_manage.php
@@ -46,6 +46,7 @@ if ( isset( $_POST['post-rent'] ) ) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/table-style.css">
     <style>
         @import "../css/pallete.css";
 
